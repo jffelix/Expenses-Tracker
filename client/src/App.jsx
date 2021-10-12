8ecfb908430f156903a5e1bb4309a3670bde5500
Jean-Luc Felix <jlfelix522@yahoo.com>
2021-10-12 02:22:22 -0700

    Modifed seeds.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,10 +13,16 @@ class App extends React.Component {
             // itemList: [],
             wasIntroButtonClicked: false,
             inputItem: '',
-            inputPrice: ''
+            inputPrice: '',
+            totalPrice: null
         }
 
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.addTotal = this.addTotal.bind(this);
+    }
+
+    componentDidMount() {
+        this.addTotal();
     }
 
     handleInputChange(event) {
@@ -29,12 +35,25 @@ class App extends React.Component {
 
     }
 
+    addTotal(array) {
+        var totalPrice = 0;
+
+        this.state.itemList.forEach(item => {
+            totalPrice += item.price
+        })
+
+        this.setState({
+            totalPrice: totalPrice.toFixed(2)
+        })
+    }
+
     render() {
         return (
             <div>
                 <h2>Welcome to Expenses Tracker from App Component!</h2>
                 <InputForm />
                 <EntryList list={this.state.itemList} />
+                <h2>Total: {this.state.totalPrice}</h2>
             </div>
         )
     }
--- server/seeds.js
+++ server/seeds.js
@@ -1,22 +1,27 @@
 var entrySeeds = [
     {
-        item: 'Subway',
-        price: 8.50,
+        item: 'AWS Billing',
+        price: 0.99,
         date: 'October 2, 2021'
     },
     {
-        item: 'Pizza',
-        price: 11.00,
+        item: 'Backpack',
+        price: 37.69,
         date: 'October 2, 2021'
+    },
+    {
+        item: 'Car Service',
+        price: 1090.35,
+        date: 'October 5, 2021'
     },   
     {
-        item: 'Groceries (Ralphs)',
-        price: 102.87,
+        item: 'Halloween Horror Nights',
+        price: 103.00,
         date: 'October 9, 2021'
     },
     {
-        item: 'Subway',
-        price: 8.50,
+        item: 'Blender',
+        price: 69.99,
         date: 'October 12, 2021'
     },    
 ];
