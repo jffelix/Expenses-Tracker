cda605be21da327da6dd1faf5967f1b883c8b9d4
Jean-Luc Felix <jlfelix522@yahoo.com>
2021-10-25 16:35:55 -0700

    Began work on Mongoose Schema in models folder

---
+++ server/models/index.js
@@ -0,0 +1,29 @@
+
+const mongoose = require('mongoose');
+const itemList = require('../seeds.js');
+
+const expensesItemSchema = new mongoose.Schema({
+    item: {
+        type: String,
+        unique: true
+    },
+    price: {
+        type: Number
+    },
+    date: {
+        type: String
+    }
+});
+
+var ExpensesItem = mongoose.model('Item', expensesItemSchema);
+
+ExpensesItem.create(itemList)
+.then(response => {
+    console.log('response: ', response);
+    console.log('Populated collection!');
+})
+.catch(err => {
+    console.log('Error received at ExpensesItem.create in models');
+})
+
+module.exports.expensesItem = ExpensesItem;
